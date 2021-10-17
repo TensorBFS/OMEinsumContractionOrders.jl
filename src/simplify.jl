7aba560e16c643ca8836d0b8e2cfa3cf11193a8e
GiggleLiu <cacate0129@gmail.com>
2021-10-17 17:49:09 -0400

    new simplifier

--- src/simplify.jl
+++ src/simplify.jl
@@ -1,11 +1,11 @@
 using OMEinsum: StaticEinCode, DynamicEinCode
-export merge_vectors
+export merge_vectors, embed_simplifier
 
 struct VectorRemover
     operations::Vector{NestedEinsum}
 end
 
-function merge_vectors(code::EinCode)
+function merge_vectors(@nospecialize(code::EinCode))
     ixs = OMEinsum.getixs(code)
     mask = trues(length(ixs))
     ops = [NestedEinsum((i,), _similar(code, (ix,), ix)) for (i,ix) in enumerate(ixs)]
@@ -33,3 +33,27 @@ function apply_simplifier(s::VectorRemover, xs)
     end
 end
 (s::VectorRemover)(xs...) = apply_simplifier(s, xs)
+
+function embed_simplifier(code::NestedEinsum, simplifier)
+    NestedEinsum(map(code.args) do arg
+        embed_simplifier(arg, simplifier)
+    end, code.eins)
+end
+
+function embed_simplifier(code::Integer, simplifier::VectorRemover)
+    op = simplifier.operations[code]
+    return unwrap_identity(op)
+end
+
+function unwrap_identity(op::NestedEinsum)
+    ixs = getixs(op.eins)
+    if length(ixs) == 1 && ixs[1] == getiy(op.eins)  # identity
+        if op.args[1] isa Integer
+            return op.args[1]
+        else
+            return unwrap_identity(op.args[1])
+        end
+    else
+        return NestedEinsum(unwrap_identity.(op.args), op.eins)
+    end
+end
