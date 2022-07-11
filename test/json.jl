363e8df919d34e5b415ec4abc663dfd81dbde76f
GiggleLiu <cacate0129@gmail.com>
2022-07-10 23:03:51 -0400

    fix json test

--- test/json.jl
+++ test/json.jl
@@ -8,8 +8,8 @@ using Test, OMEinsumContractionOrders
         for optcode in [optimize_code(code, uniformsize(code, 2), GreedyMethod()),
             optimize_code(code, uniformsize(code, 2), TreeSA(nslices=1))]
             filename = tempname()
-            writejson(filename, optcode)
-            code2 = readjson(filename)
+            OMEinsumContractionOrders.writejson(filename, optcode)
+            code2 = OMEinsumContractionOrders.readjson(filename)
             @test optcode == code2
         end
     end
