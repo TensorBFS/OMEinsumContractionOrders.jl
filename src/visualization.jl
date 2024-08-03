5d073b64bcfbd9f7e73d464f086d313628201f61
ArrogantGao <gaoxuanzhao@gmail.com>
2024-08-03 15:29:27 +0800

    revise error message

--- src/visualization.jl
+++ src/visualization.jl
@@ -1,15 +1,15 @@
 function ein2hypergraph(args...; kwargs...)
-    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`"))
+    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`."))
 end
 
 function ein2elimination(args...; kwargs...)
-    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`"))
+    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`."))
 end
 
 function viz_eins(args...; kwargs...)
-    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`"))
+    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`."))
 end
 
 function viz_contraction(args...; kwargs...)
-    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`"))
+    throw(ArgumentError("Extension `LuxorTensorPlot` not loaeded, please load it first by `using LuxorGraphPlot`."))
 end
