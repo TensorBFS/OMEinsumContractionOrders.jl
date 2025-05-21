bbf87a75c9e010644ecba600f292b3dd330af65b
Richard Samuelson <samuelson.rick@gmail.com>
2025-05-20 23:32:00 -0400

    various changes

--- src/reduction.jl
+++ src/reduction.jl
@@ -1,13 +1,80 @@
-struct Reduction{O <: CodeOptimizer} <: CodeOptimizer
+struct Reduction{S, O <: CodeOptimizer} <: CodeOptimizer
     inner::O
 end
 
-struct ReductionEA{O <: CodeOptimizer} <: CliqueTrees.EliminationAlgorithm
+function Reduction{S}(inner::O) where {O <: CodeOptimizer}
+    return Reduction{S, O}(inner)
+end
+
+function Reduction(inner::CodeOptimizer)
+    return Reduction{3}(inner)
+end
+
+struct ReductionEA{S, O <: CodeOptimizer} <: CliqueTrees.EliminationAlgorithm
     inner::O
 end
 
-#=
-function CliqueTrees.permutation(weights::AbstractVector, graph, alg::ReductionEA)
+function ReductionEA{S}(inner::O) where {O <: CodeOptimizer}
+    return ReductionEA{S, O}(inner)
+end
+
+# rules applied:
+#  - islet
+#  - twig
+#  - series
+#  - triangle
+#  - buddy
+#  - cube
+function CliqueTrees.permutation(weights::AbstractVector, graph, alg::ReductionEA{1})
+    # reduce graph
+    width = CliqueTrees.lowerbound(weights, graph)
+    graph, stack, label, width = CliqueTrees.pr3(weights, graph, width)
+    
+    # feed reduced graph back to OMEinsumContractionOrders
+    code = EinCode(maximal_cliques(graph), Int[])
+    sizes = Dict{Int, Int}(v => round(Int, 2^weights[label[v]]) for v in CliqueTrees.vertices(graph))
+    opt = optimize_code(code, sizes, alg.inner).eins
+    
+    # compute ordering
+    append!(stack, label[eincode2order(opt)])
+    return stack, invperm(stack)
+end
+
+# rules applied:
+#  - islet
+#  - twig
+#  - series
+#  - triangle
+#  - buddy
+#  - cube
+#  - simplicial
+#  - almost-simplicial
+function CliqueTrees.permutation(weights::AbstractVector, graph, alg::ReductionEA{2})
+    # reduce graph
+    width = CliqueTrees.lowerbound(weights, graph)
+    graph, stack, label, width = CliqueTrees.pr4(graph, weights)
+    
+    # feed reduced graph back to OMEinsumContractionOrders
+    code = EinCode(maximal_cliques(graph), Int[])
+    sizes = Dict{Int, Int}(v => round(Int, 2^weights[label[v]]) for v in CliqueTrees.vertices(graph))
+    opt = optimize_code(code, sizes, alg.inner).eins
+    
+    # compute ordering
+    append!(stack, label[eincode2order(opt)])
+    return stack, invperm(stack)
+end
+
+# rules applied:
+#  - islet
+#  - twig
+#  - series
+#  - triangle
+#  - buddy
+#  - cube
+#  - simplicial
+#  - almost-simplicial
+#  - indistinguishable neighbors
+function CliqueTrees.permutation(weights::AbstractVector, graph, alg::ReductionEA{3})
     # reduce graph
     width = CliqueTrees.lowerbound(weights, graph)
     weights, graph, stack, index, width = CliqueTrees.saferules(weights, graph, width)
@@ -22,21 +89,6 @@ function CliqueTrees.permutation(weights::AbstractVector, graph, alg::ReductionE
         append!(stack, CliqueTrees.neighbors(index, v))
     end
     
-    return stack, invperm(stack)
-end
-=#
-
-function CliqueTrees.permutation(weights::AbstractVector, graph, alg::ReductionEA)
-    # reduce graph
-    kernel, stack, label, width = CliqueTrees.pr4(graph, CliqueTrees.lowerbound(graph))
-    
-    # feed reduced graph back to OMEinsumContractionOrders
-    code = EinCode(maximal_cliques(kernel), Int[])
-    sizes = Dict{Int, Int}(v => round(Int, 2^weights[label[v]]) for v in CliqueTrees.vertices(kernel))
-    opt = optimize_code(code, sizes, alg.inner).eins
-    
-    # compute ordering
-    append!(stack, label[eincode2order(opt)])
     return stack, invperm(stack)
 end
 
