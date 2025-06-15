e48f22a9bdc1cb9319b0e5cf767de6d630b226fc
Richard Samuelson <samuelson.rick@gmail.com>
2025-06-15 19:52:55 -0400

    Add optimizer `HyperND`.

---
+++ src/hypernd.jl
@@ -0,0 +1,86 @@
+"""
+    HyperND(dis, algs;
+        level = 6,
+        width = 120,
+        imbalances = 130:130,
+    )
+
+Nested-dissection based optimizer. Recursively partitions a tensor network, then calls a
+greedy algorithm on the leaves. The optimizer is run a number of times: once for each greedy
+algorithm in `algs` and each imbalance value in `imbalances`. The recursion depth is controlled by
+the parameters `level` and `width`.
+
+The line graph is partitioned using the algorithm `dis`. OMEinsumContractionOrders currently supports two partitioning
+algorithms, both of which require importing an external library.
+
+| type                                                                                             | package                                              |
+|:-------------------------------------------------------------------------------------------------|:-----------------------------------------------------|
+| [`METISND`](https://algebraicjulia.github.io/CliqueTrees.jl/stable/api/#CliqueTrees.METISND)     | [Metis.jl](https://github.com/JuliaSparse/Metis.jl)  |
+| [`KaHyParND`](https://algebraicjulia.github.io/CliqueTrees.jl/stable/api/#CliqueTrees.KaHyParND) | [KayHyPar.jl](https://github.com/kahypar/KaHyPar.jl) |
+
+The optimizer is implemented using the tree decomposition library
+[CliqueTrees.jl](https://github.com/AlgebraicJulia/CliqueTrees.jl).
+
+# Arguments
+
+  - `dis`: [graph partitioning algorithm](https://algebraicjulia.github.io/CliqueTrees.jl/stable/api/#CliqueTrees.DissectionAlgorithm)
+  - `algs`: tuple of [elimination algorithms](https://algebraicjulia.github.io/CliqueTrees.jl/stable/api/#Elimination-Algorithms).
+  - `level`: maximum level
+  - `width`: minimum width
+  - `imbalances`: imbalance parameters 
+
+"""
+struct HyperND{D, A} <: CodeOptimizer
+    dis::D
+    algs::A
+    level::Int
+    width::Int
+    imbalances::StepRange{Int, Int}
+end
+
+function HyperND(dis::D=KaHyParND(), algs::A = (MF(), MMD());
+        level::Integer = 6,
+        width::Integer = 120,
+        imbalances::AbstractRange=130:130,
+    ) where{D, A}
+
+    return HyperND{D, A}(dis, algs, level, width, imbalances)
+end
+
+function optimize_hyper_nd(optimizer::HyperND, code, size_dict)
+    dis = optimizer.dis
+    algs = optimizer.algs
+    level = optimizer.level
+    width = optimizer.width
+    imbalances = optimizer.imbalances
+
+    mintc = minsc = minrw = typemax(Float64)
+    mincode = nothing
+
+    for alg in algs, imbalance in imbalances
+        curalg = SafeRules(ND(alg, dis; imbalance))
+        curoptimizer = Treewidth(; alg=curalg)
+        curcode = _optimize_code(code, size_dict, curoptimizer)
+        curtc, cursc, currw = __timespacereadwrite_complexity(curcode, size_dict)
+
+        if minsc > cursc || (minsc == cursc && mintc > curtc) || (minsc == cursc && mintc == curtc && minrw > currw)
+            mintc, minsc, minrw, mincode = curtc, cursc, currw, curcode
+        end
+    end
+
+    return mincode
+end
+
+function Base.show(io::IO, ::MIME"text/plain", optimizer::HyperND{D, A}) where {D, A}
+    println(io, "HyPar{$D, $A}:")
+    show(IOContext(io, :indent => 4), "text/plain", optimizer.dis)
+
+    for alg in optimizer.algs
+        show(IOContext(io, :indent => 4), "text/plain", alg)
+    end
+
+    println(io, "    level: $(optimizer.level)")
+    println(io, "    width: $(optimizer.width)")
+    println(io, "    imbalances: $(optimizer.imbalances)")
+    return
+end
